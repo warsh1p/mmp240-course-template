73b73a53c25ed8214b882d9f984c1369b7e7efd3
warsh1p <119904777+warsh1p@users.noreply.github.com>
2024-05-14 16:32:36 +0000

    edididh

---
+++ final/showmenu.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    var  menu = document.getElementById("top-menu");
+    if  (menu.style.display==="none" || menu.style.display === ''){
+        menu.style.display="block";
+    } else {
+    menu.style.display="none";
+        }
+    }
